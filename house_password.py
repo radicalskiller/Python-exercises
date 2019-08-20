f521a783f060c8093e3b9cf898346f7e0d9a4670
radicalskiller <39231911+radicalskiller@users.noreply.github.com>
2019-08-20 11:28:39 +0300

    Update house_password.py

--- house_password.py
+++ house_password.py
@@ -3,6 +3,7 @@
 Input: A password as a string.
 
 Output: Is the password safe or not as a boolean or any data type that can be converted and processed as a boolean. In the results you will see the converted results."""
+
 import re
 
 DIGIT_RE = re.compile('\d')
