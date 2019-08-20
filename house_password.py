082ad0320579484884956961c7fee4f03e776437
radicalskiller <39231911+radicalskiller@users.noreply.github.com>
2019-08-20 11:28:10 +0300

    Update house_password.py

--- house_password.py
+++ house_password.py
@@ -1,3 +1,8 @@
+"""Stephan and Sophia forget about security and use simple passwords for everything. Help Nikola develop a password security check module. The password will be considered strong enough if its length is greater than or equal to 10 symbols, it has at least one digit, as well as containing one uppercase letter and one lowercase letter in it. The password contains only ASCII latin letters or digits.
+
+Input: A password as a string.
+
+Output: Is the password safe or not as a boolean or any data type that can be converted and processed as a boolean. In the results you will see the converted results."""
 import re
 
 DIGIT_RE = re.compile('\d')
