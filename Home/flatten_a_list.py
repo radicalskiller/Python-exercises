c6f0877b808febf84d1616dd5ac51612aaa79464
radicalskiller <39231911+radicalskiller@users.noreply.github.com>
2019-08-22 14:33:54 +0300

    Add files via upload

---
+++ Home/flatten_a_list.py
@@ -0,0 +1,23 @@
+"""There is a list which contains integers or other nested lists which may contain yet more lists and integers which then… you get the idea. You should put all of the integer values into one flat list. The order should be as it was in the original list with string representation from left to right.
+
+We need to hide this program from Nikola by keeping it small and easy to hide. Because of this, your code should be shorter than 140 characters (with whitespaces).
+
+Input data: A nested list with integers.
+Output data: The one-dimensional list with integers.
+Precondition: 0 ≤ |array| ≤ 100
+∀ x ∈ array : -232 < x < 232 or x is a list
+depth < 10"""
+
+def flat_list(array):
+    if array == []:
+        return array
+    if isinstance(array[0], list):
+        return flat_list(array[0]) + flat_list(array[1:])
+    return array[:1] + flat_list(array[1:])
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
