6e67939395b3631a7cb4fff3e859bd4598e59466
radicalskiller <39231911+radicalskiller@users.noreply.github.com>
2019-08-25 12:58:12 +0300

    Add files via upload

---
+++ Elementary/easy_unpack.py
@@ -0,0 +1,21 @@
+"""Your mission here is to create a function that gets a tuple and returns a tuple with 3 elements - the first, third and second to the last for the given array.
+
+Input: A tuple, at least 3 elements long.
+Output: A tuple."""
+
+
+def easy_unpack(elements: tuple) -> tuple:
+    """
+        returns a tuple with 3 elements - first, third and second to the last
+    """
+    return (*[elements[0], elements[2], elements[len(elements)-2]], )
+
+
+if __name__ == '__main__':
+    print('Examples:')
+    print(easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)))
+
+    assert easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)) == (1, 3, 7)
+    assert easy_unpack((1, 1, 1, 1)) == (1, 1, 1)
+    assert easy_unpack((6, 3, 7)) == (6, 7, 3)
+    print('Done! Go Check!')
