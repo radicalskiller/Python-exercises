5a47c7dfb420ffea3bf4ea1d914553ceb1718295
radicalskiller <39231911+radicalskiller@users.noreply.github.com>
2019-08-25 15:35:33 +0300

    Update and rename easy_unpack.py to easy_unpack_v1.py

--- Elementary/easy_unpack_v1.py
+++ Elementary/easy_unpack_v1.py
@@ -5,9 +5,6 @@ Output: A tuple."""
 
 
 def easy_unpack(elements: tuple) -> tuple:
-    """
-        returns a tuple with 3 elements - first, third and second to the last
-    """
     return (*[elements[0], elements[2], elements[len(elements)-2]], )
 
 
